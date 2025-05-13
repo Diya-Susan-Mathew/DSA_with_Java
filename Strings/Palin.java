0f3cb3bf2b6346967094e1c6e1c42e0399ea5b0b
Diya-Susan-Mathew <susanmathewdiya32@gmail.com>
2025-05-13 21:46:53 +0530

    files added

--- Strings/Palin.java
+++ Strings/Palin.java
@@ -3,21 +3,29 @@ import java.util.Scanner;
 public class Palin{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
         String st = sc.nextLine();
+        if(isPalindrome(st)){
+            System.out.println("String is Palindrome");
+        }else{
+            System.out.println("String is not Palindrome");
+        }
+        
+    }
+    static boolean isPalindrome(String st){
         st = st.toLowerCase();
+        if(st == null || st.length() == 0){ // if order is in reverse , then nullpointerexception occurs
+            return true;
+        }
         boolean isPalin = true;
         for(int i=0;i<st.length();i++){
             int start = st.charAt(i);
             int end = st.charAt(st.length()-1-i);
             if(start != end){
-                isPalin = false;
+                return false;
             }
         }
-        if(isPalin){
-            System.out.println("Entered string is palindrome");
-        }else{
-            System.out.println("Entered String is not palindrome");
-        }
+        return true;
 
     }
 }
